467d3863ee5e1e2894d3a145c0659490ca4b0ca3
Anshika Saklani <166739901+Anshika-111105@users.noreply.github.com>
2024-04-18 01:59:43 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    printf("...................WELCOME TO THE GAME 'ALPHABETICAL ANTICS'...................\n");
-    printf(".........................presented by Anshika Saklani...................\n");
+    printf("\n...................WELCOME TO THE GAME 'ALPHABETICAL ANTICS'...................\n");
+    printf("\n.........................presented by Anshika Saklani...................\n");
     int p=1;
     int choice;
     while(p){                                                                    
-        printf("\nSELECT THE WORDS CATEGORIES FROM GIVEN OPTION\n ");
+        printf("\nSELECT THE WORDS CATEGORIES FROM GIVEN OPTION\n ");                                               //Word Categories given in the word game
         printf("\n1. Isogram\n(A word in which no letter of the alphabet occurs more than once.)\n");
         printf("2. Palindrome\n(A word that reads the same backwards)\n");
         printf("3. Anagram\n(A word or phrase formed by rearranging the letters of another word or phrase.)\n");
@@ -14,9 +14,9 @@ int main(){
         printf("5. Tautonym\n(A word or name made up of two identical parts, such as so-so, tom-tom )\n");
         printf(" ");
         char input[100];
-        printf("Enter the word:");
+        printf("Enter the word:");                                    //Input variable for word
         scanf("%[^\n]s",input);
-        printf("Enter the choice:");
+        printf("Enter the choice:");                                  //variable to choose category from the above menu
         scanf("%d",&choice);
         if (choice==1){
             int i, j, isIsogram = 1;
@@ -44,8 +44,8 @@ int main(){
             
         }
         else if(choice ==2){
-            int i, len, flag = 1;
-            len = strlen(input);
+            int i, len, flag = 1;                                   //flag as a counter variable
+            len = strlen(input);                                    
             for (i = 0; i < len / 2; i++) {
                 if (input[i] != input[len - i - 1]) {
                     flag = 0;
@@ -62,12 +62,12 @@ int main(){
         else if(choice==3){
             char str1[100];
             int len1, len2, i, j, found;
-            printf("Enter another word that need to be checked: ");
+            printf("\nEnter another word that need to be checked: ");
             scanf("%s", str1);
             len1 = strlen(input);
             len2 = strlen(str1);
             if (len1 != len2) {
-                printf("Not Anagrams\n");
+                printf("%s and %s are not Anagrams\n",input,str1);
                 return 0;
                 
             }
@@ -75,24 +75,24 @@ int main(){
                 found = 0;
                 for (j = 0; j < len2; j++) {
                     if (input[i] == str1[j]) {
-                        found = 1;
+                        found = 1;                                //counter variable
                         break;
                         
                     }
                 }
             }
             if (found == 0) {
-                printf("Not Anagrams\n");
+                printf("%s and %s are not Anagrams\n",input,str1);
                 return 0;
             }else{
-                printf("Anagrams\n");
+                printf("%s and %s are Anagrams\n",input,str1);
                 
             }
         }
         else if(choice==4){
             int i = 0,f=0;
             char text;
-            printf("Enter your text (without the letter 'e'): ");
+            printf("Enter your text (without the letter 'e' let say): ");
             scanf("%c", &text);
             while (input[i] != '\0') {
                 if (input[i] != text ) {
@@ -120,6 +120,16 @@ int main(){
             }
         }
     printf("Enter 1 to continue or 0 t exit:");
+     scanf("%d",&p);
+    }
+    return 0;
+}
+
+
+
+
+
+        
     scanf("%d",&p);
     }
     return 0;
